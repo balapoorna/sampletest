324482a9a83a85494ff8327a18fa5937895b2e54
balapoorna <balapoorna.krishna@gmail.com>
2021-06-14 16:12:16 +0530

    addition Scripts

--- src/org/company/Company.java
+++ src/org/company/Company.java
@@ -5,12 +5,23 @@ import org.client.Client;
 public class Company extends Client {
 	public void companyName() {
 		System.out.println(" Company Name is : TCS ");
-		
+
 	}
+
+	public void companyAddress2() {
+		System.out.println("Thuraipakkam");
+	}
+
+	public void phonoNumber2() {
+		System.out.println("9677611441");
+	}
+
 	public static void main(String[] args) {
-		Company c=new Company();
+		Company c = new Company();
 		c.companyName();
+		c.companyAddress2();
 		c.clientName();
+		c.phonoNumber2();
 	}
 
 }
